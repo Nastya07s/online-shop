bc8712aef854636753207c9deb8b4b89a765fbe8
Nastya07s <nastya.kotovich07@gmail.com>
2020-03-02 10:06:13 +0300

    feat: add slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,12 @@
-$(function(){
-
-
-
-  
+$(document).ready(function () {
+  $('.slider-items').slick({
+    autoplay: true,
+    autoplaySpeed: 2000,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    variableWidth: true,
+    prevArrow: $('.prev'),
+    nextArrow: $('.next'),
+  });
 });
